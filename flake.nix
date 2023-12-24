ca067907420fa44cf23693a48e6873b95a9f2152
dfense <andreas@soulboarder.net>
2023-12-24 09:29:37 +0100

    initial homemanager config, included .bashrc

--- flake.nix
+++ flake.nix
@@ -12,16 +12,19 @@
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
     # Official NixOS package source, using nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # home-manager, used for managing user configuration
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+    # home-manager = {
+    # url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
       # to avoid problems caused by different versions of nixpkgs.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   # `outputs` are all the build result of the flake.
@@ -34,7 +37,7 @@
   # 
   # The `@` syntax here is used to alias the attribute set of the
   # inputs's parameter, making it convenient to use inside the function.
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       # By default, NixOS will try to refer the nixosConfiguration with
       # its hostname, so the system named `nixos-test` will use this one.
@@ -47,7 +50,7 @@
       # Run the following command in the flake's directory to
       # deploy this configuration on any NixOS system:
       #   sudo nixos-rebuild switch --flake .#nixos-test
-      "nixos-minix" = nixpkgs.lib.nixosSystem {
+      nixos-minix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         # The Nix module system can modularize configuration,
@@ -94,6 +97,17 @@
           # old configuration file can still take effect.
           # Note: configuration.nix itself is also a Nixpkgs Module,
           ./configuration.nix
+	  # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.dfense= import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+	}
         ];
       };
     };
--- home.nix
+++ home.nix
@@ -147,48 +147,47 @@
 
     # set some aliases, feel free to add more or remove some
     shellAliases = {
-      k = "kubectl";
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
 
-      rp='resticprofile'
+      rp="resticprofile";
 
       # neovim
-      vim="nvim"
-      nv="nvim"
-      vom="nvim"
-      nvom="nvim"
+      vim="nvim";
+      nv="nvim";
+      vom="nvim";
+      nvom="nvim";
       
-      ls="ls --color=auto"
-      ll="ls -lav --ignore=.."   # show long listing of all except ".."
-      l="ls -lav --ignore=.?*"   # show long listing but no hidden dotfiles except "."
-      e="exa -F"
-      el="exa -l --icons -gHmhb"
-      ela="exa -la --icons -gHmhb"
-      et1="exa -T --level=1 --icons -gHmhb"
-      et2="exa -T --level=2 --icons -gHmhb"
-      et3="exa -T --level=3 --icons -gHmhb"
-      et="exa -T --icons -gHmhb"
-      etl1="exa -lT --level=1 --icons -gHmhb"
-      etl2="exa -lT --level=2 --icons -gHmhb"
-      etl3="exa -lT --level=3 --icons -gHmhb"
-      etl="exa -lT --icons -gHmhb"
+      ls="ls --color=auto";
+      ll="ls -lav --ignore=..";   # show long listing of all except "..";
+      l="ls -lav --ignore=.?*";   # show long listing but no hidden dotfiles except ".";
+      e="exa -F";
+      el="exa -l --icons -gHmhb";
+      ela="exa -la --icons -gHmhb";
+      et1="exa -T --level=1 --icons -gHmhb";
+      et2="exa -T --level=2 --icons -gHmhb";
+      et3="exa -T --level=3 --icons -gHmhb";
+      et="exa -T --icons -gHmhb";
+      etl1="exa -lT --level=1 --icons -gHmhb";
+      etl2="exa -lT --level=2 --icons -gHmhb";
+      etl3="exa -lT --level=3 --icons -gHmhb";
+      etl="exa -lT --icons -gHmhb";
       
       #git aliases
       #gh="git log --pretty=format:"%h %ad | %s%d" --graph --date=short"
-      gh="git log --graph --pretty=format:"%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %s""
-      gha="git log --graph --pretty=format:"%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %s" --all"
-      gs="git status "
-      ga="git add "
-      gb="git branch "
-      gc="git commit"
-      gd="git diff"
-      gr="git rm"
-      gco="git checkout "
-      gty="git cat-file -t "
-      gdu="git cat-file -p "
-      got="git "
-      get="git "
+      gh="git log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %s'";
+      gha="git log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %s' --all";
+      gs="git status ";
+      ga="git add ";
+      gb="git branch ";
+      gc="git commit";
+      gd="git diff";
+      gr="git rm";
+      gco="git checkout ";
+      gty="git cat-file -t ";
+      gdu="git cat-file -p ";
+      got="git ";
+      get="git ";
     };
   };
 
