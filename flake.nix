10c38ce8e9e85e2bdf038e1c338e2ff1e16b1594
dfense <andreas@soulboarder.net>
2023-12-24 11:01:24 +0100

    fixing version mismatch between nixpkgs and homemanager for caf9318

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     # Official NixOS package source, using nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # home-manager, used for managing user configuration
     # home-manager = {
