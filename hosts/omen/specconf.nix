aea5c1ad609abcc13455bd2a01e10e6ca2fba652
dfense <andreas@soulboarder.net>
2023-12-27 20:44:53 +0100

    initial conf for omen

--- hosts/omen/specconf.nix
+++ hosts/omen/specconf.nix
@@ -5,11 +5,11 @@
 { config, pkgs, ... }:
 
 {
-  # boot.kernelPackages = pkgs.linuxPackages_latest # for latest kernel
-  boot.kernelPackages =
-    pkgs.linuxKernel.packages.linux_5_4; # kernel downgrade because of ACPI errors on minix with latest kernel (ic2)
+  boot.kernelPackages = pkgs.linuxPackages_latest; # for latest kernel
+#  boot.kernelPackages =
+#    pkgs.linuxKernel.packages.linux_5_4; # kernel downgrade because of ACPI errors on minix with latest kernel (ic2)
 
-  networking.hostName = "minix"; # Define your hostname.
+  networking.hostName = "omen"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
